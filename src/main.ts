1f7176cb405d14fda9ca7932c1ccd20315b69c55
PenguinQ <pvpgns@yahoo.com>
2025-11-15 23:17:15 +0700

    feat: update landing page

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,8 @@
-const debounce = (callback: (...args: any) => void, delay = 300) => {
-  let timeout: ReturnType<typeof setTimeout>;
-
-  return (...args: any) => {
-    clearTimeout(timeout);
-
-    timeout = setTimeout(() => {
-      callback(...args);
-    }, delay);
-  };
-};
-
 document.addEventListener('DOMContentLoaded', () => {
-  /**
-   * -------------------
-   * Header menu scripts
-   * -------------------
-   */
-  const navToggle       = document.getElementsByClassName('header-nav-toggle')[0];
-  const navColumn       = document.getElementsByClassName('header-nav--col')[0];
-  const navLinks        = document.querySelectorAll('.header-nav a');
-  const navButtons      = document.querySelectorAll('.header-nav .button');
+   const animatedEl    = document.getElementById('hero-typing-text');
+   const animatedTexts = ['Cari', 'Temukan', 'Beli', 'Jual'];
 
-  const toggleMenuState = (state: boolean) => {
-    if (state) {
-      navToggle.removeAttribute('data-active');
-      navColumn.removeAttribute('data-active');
-    } else {
-      navToggle.setAttribute('data-active', '');
-      navColumn.setAttribute('data-active', '');
-    }
-  };
-
-  const handleWindowResize = debounce(() => {
-    if (window.innerWidth >= 768) {
-      const isNavToggleActive = navToggle.hasAttribute('data-active');
-      const isNavColumnActive = navColumn.hasAttribute('data-active');
-
-      if (isNavToggleActive && isNavColumnActive) toggleMenuState(true);
-    }
-  });
-
-  window.addEventListener('resize', handleWindowResize);
-
-  navToggle?.addEventListener('click', () => {
-    const isActive = navToggle.hasAttribute('data-active');
-
-    toggleMenuState(isActive);
-  });
-
-  for (const link of navLinks) {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      const target        = e.target as HTMLAnchorElement;
-      const targetHref    = target.getAttribute('href') as string;
-      const targetElement = document.querySelector(targetHref) as HTMLElement;
-
-      targetElement?.scrollIntoView({ behavior: 'smooth' });
-      toggleMenuState(true);
-    });
-  }
-
-  for (const button of navButtons) {
-    button.addEventListener('click', () => {
-      toggleMenuState(true);
-    });
-  }
-
-  /**
-   * ---------------------
-   * Intro section scripts
-   * ---------------------
-   */
-   const introNextButton  = document.getElementById('intro-next');
-   const introChatElement = document.getElementById('intro-chat-text');
-   const introTexts = [
-    'Sure, I think we can do it!',
-    'Well, I think this one better...',
-    'I guess so? Why don\'t you contact Tono first for confirmation?',
-    'Probably, but looks like the product isn\'t available right now.',
-  ];
-
-   const animateTexts = (texts: string[], element: HTMLElement) => {
+   const animateText = (texts: string[], element: HTMLElement) => {
     let textIndex  = 0;
     let typingText = '';
     let isTyping   = true;
@@ -97,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
           setTimeout(() => {
             isTyping = false;
             animate();
-          }, 1500);
+          }, 2000);
         }
       } else {
         if (typingText.length > 0) {
@@ -119,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // @ts-ignore
   VANTA.DOTS({
-    el: "#intro-container",
+    el             : "#main",
     mouseControls  : false,
     touchControls  : false,
     gyroControls   : false,
@@ -132,51 +53,5 @@ document.addEventListener('DOMContentLoaded', () => {
     backgroundAlpha: 0,
   });
 
-  if (introChatElement) animateTexts(introTexts, introChatElement);
-
-  introNextButton?.addEventListener('click', () => {
-    const target = introNextButton.getAttribute('data-target');
-
-    if (target) {
-      const targetElement = document.querySelector(target) as HTMLElement;
-
-      targetElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-
-  /**
-   * Pricing section
-   */
-  // @ts-ignore
-  new Swiper('.pricing-list', {
-    slidesPerView : 1,
-    spaceBetween  : 24,
-    centeredSlides: false,
-    breakpoints   : {
-      320: {
-        slidesPerView : 1.2,
-        centeredSlides: true,
-      },
-      768: {
-        slidesPerView : 2.2,
-        centeredSlides: false,
-      },
-      1024: {
-        slidesPerView : 4,
-        centeredSlides: false,
-      }
-    },
-  });
-
-  /**
-   * Testimonial section
-   */
-  // @ts-ignore
-  new Swiper('.testimonial-list', {
-    slidesPerView: 1,
-    effect: 'cards',
-    cardsEffect: {
-      slideShadows: false,
-    },
-  });
+  if (animatedEl) animateText(animatedTexts, animatedEl);
 });
